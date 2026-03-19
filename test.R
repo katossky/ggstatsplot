b7a047ea3c63de97d5026893108bd50e931e9ef7
MALET Julie <id2591@clust-n13.domensai.ecole>
2026-03-19 11:08:04 +0100

    Test et visualisation des erreurs liées à l'issue

--- test.R
+++ test.R
@@ -1,1 +1,62 @@
-# Test
+# install.packages("ggstatsplot")
+
+library(ggstatsplot)
+
+
+##### Reproduction de l'erreur d'origine
+
+(df <- dplyr::tibble(
+  grp = c(rep("c", 5), rep("a", 5), rep("b", 5)),
+  val1 = runif(15),
+  val2 = runif(15)
+))
+
+grouped_ggscatterstats(df, val1, val2, grouping.var = grp)
+# Graphiques affichés dans l'ordre alphabétique.
+
+##### Solution proposée par un répondant
+
+test <- data.frame(
+  id = c("b", "c", "a"),
+  val = 1:3
+)
+
+# reordered
+split(test, ~ id)
+
+# not reordered
+test$id <- factor(test$id, levels=unique(test$id))
+split(test, ~ id)
+
+
+#####
+
+.grouped_list <- function(data, grouping.var = NULL) {
+  data <- as_tibble(data)
+
+  if (quo_is_null(enquo(grouping.var))) {
+    return(data)
+  }
+
+  data %<>%
+    mutate(
+      across(
+        {{ grouping.var }},
+        ~ factor(.x, levels = unique(.x))
+      )
+    )
+
+  data %>% split(f = new_formula(NULL, enquo(grouping.var)), drop = FALSE)
+}
+
+#> You can cite this package as:
+#>      Patil, I. (2021). Visualizations with statistical details: The 'ggstatsplot' approach.
+#>      Journal of Open Source Software, 6(61), 3167, doi:10.21105/joss.03167
+
+(df <- dplyr::tibble(
+  grp = c(rep("c", 5), rep("a", 5), rep("b", 5)),
+  val1 = runif(15),
+  val2 = runif(15)
+))
+
+grouped_ggscatterstats(df, val1, val2, grouping.var = grp)
