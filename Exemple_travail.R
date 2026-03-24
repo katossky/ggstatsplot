d72218b79e1daca710da0747e46208a8c93bdca0
EPRINCHARD Marion <id2656@clust-n13.domensai.ecole>
2026-03-24 23:51:21 +0100

    Solution fonction correct mais erreur pour générer les graphiques

--- Exemple_travail.R
+++ Exemple_travail.R
@@ -23,40 +23,60 @@ test <- data.frame(
 # reordered
 split(test, ~ id)
 
+# la fonction split réordonne automatiquement les groupes quand ce n'est pas en factor
+
 
 # not reordered
 test$id <- factor(test$id, levels=unique(test$id))
 split(test, ~ id)
 
 
-.grouped_list <- function(data, grouping.var = NULL) {
-  data <- as_tibble(data)
 
-  if (quo_is_null(enquo(grouping.var))) {
-    return(data)
-  }
-
-  data %>%
-    mutate(
-      across(
-        {{ grouping.var }},
-        ~ factor(.x, levels = unique(.x))
-      )
-    )
-
-  data %>% split(f = new_formula(NULL, enquo(grouping.var)), drop = FALSE)
-}
-
-
-(df <- dplyr::tibble(
+# test de la solution :
+df <- dplyr::tibble(
   grp = c(rep("c", 5), rep("a", 5), rep("b", 5)),
   val1 = runif(15),
   val2 = runif(15)
-))
+)
+df
+
+
+.grouped_list <- function(data, grouping.var = NULL) {
+
+  data <- tibble::as_tibble(data)
+
+  if (rlang::quo_is_null(rlang::enquo(grouping.var))) {
+    return(data)
+  }
+
+  # Correction : conserver l'ordre d'apparition des groupes avec factor
+  data <- dplyr::mutate(
+    data,
+    dplyr::across(
+      {{ grouping.var }},
+      ~ factor(.x, levels = unique(.x))
+    )
+  )
+
+  data %>% split(
+    f = rlang::new_formula(NULL, rlang::enquo(grouping.var)),
+    drop = FALSE
+  )
+
+
+}
+
+
+assignInNamespace(".grouped_list", .grouped_list, ns = "ggstatsplot")
 
 
 .grouped_list(df, grp)
+# la fonction renvoie l'ordre correct des var
 
+
+# erreur lors de l'éxécution
 grouped_ggscatterstats(df, val1, val2, grouping.var = grp)
+
+
 
 
